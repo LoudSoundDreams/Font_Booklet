f339dc084971e408e79ff079f79307d7806591a4
Linus <linus@loudsounddreams.com>
2023-05-07 14:44:21 -0400

    Add list of samples

---
+++ Font Booklet/SampleList.swift
@@ -0,0 +1,22 @@
+//
+//  SampleList.swift
+//  Font Booklet
+//
+//  Created by h on 2023-05-07.
+//
+
+import SwiftUI
+
+struct SampleList: View {
+    var body: some View {
+		List((1..<4)) { number in
+			Text("The quick brown fox jumps over the lazy dog.")
+		}
+    }
+}
+
+struct SampleList_Previews: PreviewProvider {
+    static var previews: some View {
+        SampleList()
+    }
+}
