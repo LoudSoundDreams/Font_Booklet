b7e12cd48d1956590cf8499231c13e75f5b96746
Linus <linus@loudsounddreams.com>
2023-05-07 16:26:26 -0400

    Show pangrams with font names

--- Font Booklet/SampleList.swift
+++ Font Booklet/SampleList.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct SampleList: View {
+	private static let familiesAndMembers: [[String]] = {
+		let families = UIFont.familyNames // ["Verdana", "Futura"]
+		return families.map { family in
+			return UIFont.fontNames(forFamilyName: family) // ["Verdana", "Verdana-Bold"]
+		}
+	}()
+	private static let members: [String] = familiesAndMembers.flatMap { $0 }
+	
 	var body: some View {
-		List(UIFont.familyNames, id: \.self) { familyName in
-			Text(
-				UIFont.fontNames(forFamilyName: familyName)
-					.reduce(familyName) { partialResult, fontName in
-						partialResult + " & " + fontName
-					}
-			)
-			.font(.custom(familyName, size: 32))
+		List(Self.members, id: \.self) { fontName in
+			VStack(alignment: .leading) {
+				Text("The quick brown fox jumps over the lazy dog.")
+					.font(.custom(fontName, size: 32))
+				Text(fontName)
+					.font(.caption)
+					.foregroundColor(.secondary)
+			}
 		}
 	}
 }
