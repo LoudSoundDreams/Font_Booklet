376b66ca5057045d16e5e66819ce7af73b5c187a
Linus <linus@loudsounddreams.com>
2023-05-07 14:38:56 -0400

    Initial Commit

---
+++ Font Booklet/Font_BookletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Font_BookletApp.swift
+//  Font Booklet
+//
+//  Created by h on 2023-05-07.
+//
+
+import SwiftUI
+
+@main
+struct Font_BookletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
