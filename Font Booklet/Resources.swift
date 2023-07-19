827cb6388a8980ad3f464d978dd9d13a40611afe
Linus <linus@loudsounddreams.com>
2023-07-19 05:53:43 -0400

    Clean up code

--- Font Booklet/Resources.swift
+++ Font Booklet/Resources.swift
@@ -22,13 +22,7 @@ enum Fonts {
 	static let familyAndFaceNames: [[String]] = {
 		let families = UIFont.familyNames // ["Trebuchet MS", "Verdana"]
 		return families.map { family in
-			let result = UIFont.fontNames(forFamilyName: family) // ["Verdana", "Verdana-Italic", "Verdana-Bold", "Verdana-BoldItalic"]
-			print()
-			print(family)
-			result.forEach {
-				print($0)
-			}
-			return result
+			return UIFont.fontNames(forFamilyName: family) // ["Verdana", "Verdana-Italic", "Verdana-Bold", "Verdana-BoldItalic"]
 		}
 	}()
 	static let faceNames: [String] = familyAndFaceNames.flatMap { $0 }
