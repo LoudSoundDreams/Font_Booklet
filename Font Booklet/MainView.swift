3ded7f5ea6462bc550c8b572e94774fd17e307eb
Linus <linus@loudsounddreams.com>
2023-07-23 08:15:17 -0400

    Rearrange code

--- Font Booklet/MainView.swift
+++ Font Booklet/MainView.swift
@@ -61,62 +61,6 @@ struct BookmarkImage: View {
 	}
 }
 
-private extension View {
-	func swipeActions_ToggleBookmarked(
-		familySurname: String,
-		in bookmarked: Bookmarked
-	) -> some View {
-		swipeActions(edge: .leading) {
-			if bookmarked.familySurnames.contains(familySurname) {
-				Button {
-					bookmarked.familySurnames.remove(familySurname)
-				} label: {
-					Image(systemName: "bookmark.slash.fill")
-				}
-				.tint(.red)
-			} else {
-				Button {
-					bookmarked.familySurnames.insert(familySurname)
-				} label: {
-					Image(systemName: "bookmark.fill")
-				}
-				.tint(.red)
-				// ! Accessibility label
-			}
-		}
-	}
-}
-
-struct SampleView: View {
-	let label: String
-	let familySurname: String
-	let memberName: String
-	let sampleText: String
-	
-	@ObservedObject private var bookmarked: Bookmarked = .shared
-	
-	var body: some View {
-		HStack(alignment: .top) {
-			VStack(
-				alignment: .leading,
-				spacing: .eight
-			) {
-				Text(label)
-					.font(.caption)
-					.foregroundColor(.secondary)
-				Text(sampleText)
-					.font(.custom(
-						memberName,
-						size: .eight * 4
-					))
-			}
-			Spacer()
-			BookmarkImage(visible: bookmarked.familySurnames.contains(familySurname))
-		}
-		.swipeActions_ToggleBookmarked(familySurname: familySurname, in: bookmarked)
-	}
-}
-
 struct MainView: View {
 	@ObservedObject private var bookmarked: Bookmarked = .shared
 	@AppStorage(DefaultsKey.sampleText.rawValue) private var sample: String = Pangrams.standard
