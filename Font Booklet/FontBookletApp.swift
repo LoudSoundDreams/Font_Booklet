dd8c296362bdb17ad3aa1611ba4f3b4fccdec5f7
Linus <linus@loudsounddreams.com>
2023-07-23 07:30:25 -0400

    Delete unused `UserDefaults` entries on launch

--- Font Booklet/FontBookletApp.swift
+++ Font Booklet/FontBookletApp.swift
@@ -7,12 +7,34 @@
 
 import SwiftUI
 
-enum DefaultsKey: String {
+enum DefaultsKey: String, CaseIterable {
 	case sampleText = "SampleText"
+}
+enum DefaultsPrefix: String, CaseIterable {
+	case prefix_bookmarkedFamily = "BookmarkedFamily:"
 }
 
 @main
 struct FontBookletApp: App {
+	init() {
+		let defaults = UserDefaults.standard
+		let keysToKeep = Set(DefaultsKey.allCases.map { $0.rawValue })
+		let prefixesToKeep = DefaultsPrefix.allCases.map { $0.rawValue }
+		defaults.dictionaryRepresentation().forEach { (existingKey, _) in
+			// Keep the entry only if we’re still using it; otherwise, delete it.
+			
+			if keysToKeep.contains(existingKey) { return }
+			
+			for prefixToKeep in prefixesToKeep {
+				if existingKey.hasPrefix(prefixToKeep) {
+					return
+				}
+			}
+			
+			defaults.removeObject(forKey: existingKey)
+		}
+	}
+	
 	var body: some Scene {
 		WindowGroup {
 			MainView()
